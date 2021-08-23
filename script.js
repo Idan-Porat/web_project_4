76f98181c3a223760d483a32454d10160dd5d66b
PracticumStudent <idanx061@gmail.com>
2021-08-23 10:40:12 +0300

    fix: fix html: from <il> to <li>, popup: change the code to the same purpose, overlay: delete the block

--- script.js
+++ script.js
@@ -1,66 +1,41 @@
-function openPopup() {
-    let fullName = document.querySelector('.profile__full-name');
-    let career = document.querySelector('.profile__career');
-    let newFullName = document.querySelector('.popup__item_name');
-    let newCareer = document.querySelector('.popup__item_about');
-    newFullName.value = fullName.textContent;
-    newCareer.value = career.textContent;
-    document.querySelector(".overlay").style.display = "block";
-    document.querySelector(".popup").style.display = "flex";
-}
+let fullName = document.querySelector('.profile__full-name');
+let career = document.querySelector('.profile__career');
+let newFullName = document.querySelector('.popup__item_name');
+let newCareer = document.querySelector('.popup__item_about');
+let popup = document.querySelector(".popup");
+let popupClose = document.querySelector(".popup__close-button");
 
-
-function closePopup() {
-  let fullName = document.querySelector('.profile__full-name');
-  let career = document.querySelector('.profile__career');
-  let newFullName = document.querySelector('.popup__item_name');
-  let newCareer = document.querySelector('.popup__item_about');  
-  newFullName.value = fullName.textContent;
-  newCareer.value = career.textContent;
-  document.querySelector(".overlay").style.display = "none";
-  document.querySelector(".popup").style.display = "none";
-}
-
-let open = document.querySelector('.profile__button');
-let close = document.querySelector('.popup__close-button');
-open.addEventListener("click", openPopup);
-close.addEventListener("click", closePopup);
-
-// Let's find the form in the DOM
-let popupElement = document.querySelector('.popup')
-
-  // Next is the form submit handler, though
-  // it won't submit anywhere just yet
-  function handleFormSubmit(evt) {
-    evt.preventDefault(); // This line stops the browser from submitting the form in the default way.
-    // Having done so, we can define our own way of submitting the form.
-    // We'll explain it in more detail later.
-
-    // Let's find the form fields in the DOM
-    let nameInput = document.querySelector('.popup__item_name');
-    let jobInput = document.querySelector('.popup__item_about');
-    // Get the values of each field from the corresponding value property
-    let fullName = document.querySelector('.profile__full-name');
-    let career = document.querySelector('.profile__career');
-    // Select elements where the field values will be entered
-    fullName.textContent = nameInput.value;
-    career.textContent = jobInput.value;
-    document.querySelector(".overlay").style.display = "none";
-    document.querySelector(".popup").style.display = "none";
-    // Insert new values using the textContent property of the querySelector() method
+function openPopupOrClose() {
+  if(!(popup.classList.contains("popup__open"))) {
+    popup.classList.add("popup__open");
+    const newFullNameVal = fullName.textContent;
+    const newCareerVal = career.textContent;
+    newFullName.value = newFullNameVal;
+    newCareer.value = newCareerVal
+  } else {
+    popup.classList.remove("popup__open");
   }
-
-// Connect the handler to the form:
-// it will watch the submit event
-let changeText = document.querySelector('.popup');
-changeText.addEventListener('submit', handleFormSubmit);
-
-let likeButtons = document.querySelectorAll(".elements__like-button");
-
-function likeFunction(likeButton) {
-  likeButton.classList.toggle("elements__like-button_active");
 }
 
-for (let i = 0; i < likeButtons.length; i++) {
-  likeButtons[i].addEventListener("click", () => likeFunction(likeButtons[i]));
+function savePopup() {
+   const newFullNameVal = newFullName.value;
+   const newCareerVal =  newCareer.value;
+   fullName.textContent = newFullNameVal;
+   career.textContent = newCareerVal;
 }
+
+let openPopupClick = document.querySelector('.profile__button');
+let closePopupClick = document.querySelector('.popup__close-button');
+openPopupClick.addEventListener("click",  openPopupOrClose);
+closePopupClick.addEventListener("click",  openPopupOrClose);
+
+function handleFormSubmit(evt) { 
+    evt.preventDefault();
+    fullName.textContent = newFullName.value; 
+    career.textContent = newCareer.value;
+    openPopupOrClose();
+}
+ 
+popup.addEventListener('submit', handleFormSubmit); 
+ 
+
