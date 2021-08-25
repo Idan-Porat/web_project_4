39a414f5211a862a0364f3f59579ea9d32e25eda
PracticumStudent <idanx061@gmail.com>
2021-08-25 13:45:27 +0300

    refactor: made a template for element card in the html file and delete unnecessary, and i added the code that serves the same purpose to the js file

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let newFullName = document.querySelector('.popup__item_type_name');
 let newCareer = document.querySelector('.popup__item_type_about');
 let popup = document.querySelector(".popup");
 let popupClose = document.querySelector(".popup__close-button");
+let elementsContainer = document.querySelector(".elements__container");
 
 function openPopupOrClose() {
   if(!(popup.classList.contains("popup_open"))) {
@@ -29,6 +30,47 @@ function handleFormSubmit(evt) {
     openPopupOrClose();
 }
 let popupContainer = document.querySelector(".popup__input-container")
-popupContainer.addEventListener('submit', handleFormSubmit); 
+popupContainer.addEventListener('submit', handleFormSubmit);
+
+const initialCards = [
+  {
+    name: "New York",
+    link: "https://images.unsplash.com/photo-1466500419182-8602dc906b51?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80.jpg"
+  },
+  {
+    name: "Tunnel view, California",
+    link: "https://images.unsplash.com/photo-1604947338354-426e7a15586d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80.jpg"
+  },
+  {
+    name: "Kauai beach drive, lihue",
+    link: "https://images.unsplash.com/photo-1496155110490-cdcc48eba923?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1191&q=80.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+]; 
+
+function addElementCard() {
+  const elementsTemplate = document.querySelector("#elements__template").content;
+
+  initialCards.forEach ((item) => {
+  const cardElement = elementsTemplate.querySelector('.elements__card').cloneNode(true);  
+  cardElement.querySelector(".elements__pic").src = item.link;
+  cardElement.querySelector(".elements__title").textContent = item.name;
+  elementsContainer.append(cardElement);
+  });
+}
+addElementCard();
+
+
  
 
