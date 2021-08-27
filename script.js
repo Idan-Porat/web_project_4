f6e3d150f65a5fec3a16ee92cfe5acff78149ffc
PracticumStudent <idanx061@gmail.com>
2021-08-27 13:13:55 +0300

    refactor: add new popup style (style with diffrent name), and add to it a new feature which allows you to upload any image you want using the link address of that image, add a like button,

--- script.js
+++ script.js
@@ -2,35 +2,92 @@ let fullName = document.querySelector('.profile__full-name');
 let career = document.querySelector('.profile__career');
 let newFullName = document.querySelector('.popup__item_type_name');
 let newCareer = document.querySelector('.popup__item_type_about');
+let newImageTitle = document.querySelector('popup__item_type_image_title');
+let newImageUrl = document.querySelector('popup__item_type_image_url');
+
 let popup = document.querySelector(".popup");
 let popupClose = document.querySelector(".popup__close-button");
 let elementsContainer = document.querySelector(".elements__container");
 
-function openPopupOrClose() {
-  if(!(popup.classList.contains("popup_open"))) {
-    popup.classList.add("popup_open");
+
+function popupTemplate() {
+  const popupContainer = document.querySelector('.popup__container');
+  if (!EditClick.click) {
+    popupContainer.querySelector(".popup__header").textContent = "Edit profile";
+    popupContainer.querySelector(".popup__button").textContent = "Save";
+    popupContainer.querySelector(".popup__item_type_name").placeholder = "Name";
+    popupContainer.querySelector(".popup__item_type_about").placeholder = "About me";
     const newFullNameVal = fullName.textContent;
     const newCareerVal = career.textContent;
     newFullName.value = newFullNameVal;
-    newCareer.value = newCareerVal
+    newCareer.value = newCareerVal;
   } else {
-    popup.classList.remove("popup_open");
+    popupContainer.querySelector(".popup__header").textContent = "New Photo";
+    popupContainer.querySelector(".popup__button").textContent = "Create";
+    popupContainer.querySelector(".popup__item_type_name").placeholder = "Title";
+    popupContainer.querySelector(".popup__item_type_about").placeholder = "Image URL";
   }
 }
 
-let openPopupClick = document.querySelector('.profile__button');
+let EditClick = document.querySelector('.profile__button');
 let closePopupClick = document.querySelector('.popup__close-button');
-openPopupClick.addEventListener("click",  openPopupOrClose);
-closePopupClick.addEventListener("click",  openPopupOrClose);
+let addPhoto = document.querySelector('.profile__rectangle');
 
-function handleFormSubmit(evt) { 
-    evt.preventDefault();
-    fullName.textContent = newFullName.value; 
-    career.textContent = newCareer.value;
-    openPopupOrClose();
+EditClick.addEventListener("click", function editprofile() {
+  const popupContainer = document.querySelector('.popup__container');
+  popupContainer.querySelector(".popup__header").textContent = "Edit profile";
+  popupContainer.querySelector(".popup__button").textContent = "Save";
+  popupContainer.querySelector(".popup__item_type_name").placeholder = "Name";
+  popupContainer.querySelector(".popup__item_type_about").placeholder = "About me";
+  const newFullNameVal = fullName.textContent;
+  const newCareerVal = career.textContent;
+  newFullName.value = newFullNameVal;
+  newCareer.value = newCareerVal;
+  popup.classList.add("popup_open");
+});
+
+addPhoto.addEventListener("click", function addNewPhoto() {
+  const popupContainer = document.querySelector('.popup__container');
+  popupContainer.querySelector(".popup__header").textContent = "New Photo";
+  popupContainer.querySelector(".popup__button").textContent = "Create";
+  popupContainer.querySelector(".popup__item_type_name").placeholder = "Title";
+  popupContainer.querySelector(".popup__item_type_about").placeholder = "Image URL";
+  newFullName.value = "";
+  newCareer.value = "";
+  popup.classList.add("popup_open");
+});
+
+closePopupClick.addEventListener("click", closePopup);
+
+function closePopup() {
+  popup.classList.remove("popup_open");
 }
-let popupContainer = document.querySelector(".popup__input-container")
-popupContainer.addEventListener('submit', handleFormSubmit);
+
+let popupContainer = document.querySelector(".popup__input-container");
+popupContainer.addEventListener('submit', function saveProfileOrAddNewCard(evt) {
+  evt.preventDefault();
+  if (!(popupContainer.querySelector(".popup__item_type_name").placeholder == "Title")) {
+    fullName.textContent = newFullName.value;
+    career.textContent = newCareer.value;
+    closePopup();
+  } else {
+    addNewCard(newFullName.value, newCareer.value);
+    likebutton();
+    closePopup();
+  }
+});
+
+function addNewCard(cardTitle, urlImage) {
+  const newCard = { name: cardTitle, link: urlImage};
+  initialCards.push(newCard);
+  const elementsTemplate = document.querySelector("#elements__template").content;
+  const cardElement = elementsTemplate.querySelector('.elements__card').cloneNode(true);
+  cardElement.querySelector(".elements__pic").src = newCard.link;
+  cardElement.querySelector(".elements__title").alt = newCard.name;
+  cardElement.querySelector(".elements__title").textContent = newCard.name;
+  elementsContainer.prepend(cardElement);
+}
+
 
 const initialCards = [
   {
@@ -57,20 +114,39 @@ const initialCards = [
     name: "Lago di Braies",
     link: "https://code.s3.yandex.net/web-code/lago.jpg"
   }
-]; 
+];
+
+
 
 function addElementCard() {
   const elementsTemplate = document.querySelector("#elements__template").content;
-
-  initialCards.forEach ((item) => {
-  const cardElement = elementsTemplate.querySelector('.elements__card').cloneNode(true);  
-  cardElement.querySelector(".elements__pic").src = item.link;
-  cardElement.querySelector(".elements__title").textContent = item.name;
-  elementsContainer.append(cardElement);
+  
+  initialCards.forEach((card) => {
+    const cardElement = elementsTemplate.querySelector('.elements__card').cloneNode(true);
+    cardElement.querySelector(".elements__pic").src = card.link;
+    cardElement.querySelector(".elements__pic").alt = card.name;
+    cardElement.querySelector(".elements__title").textContent = card.name;
+    elementsContainer.append(cardElement);
   });
 }
 addElementCard();
+likebutton();
 
+function likebutton() {
+  let likeButtons = document.querySelectorAll(".elements__like-button"); 
+ 
+  function likeFunction(likeButton) { 
+    likeButton.classList.toggle("elements__like-button_active"); 
+  } 
+ 
+  for (let i = 0; i < likeButtons.length; i++) { 
+    likeButtons[i].addEventListener("click", () => likeFunction(likeButtons[i])); 
+  } 
+}
 
  
+
+
+
+
 
